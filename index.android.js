7e126290d368afc695fc03fbd3d7ad3bce2ba9a7
satvik <satvik.jagannath7@gmail.com>
2016-01-07 22:53:10 +0530

    Grid view - initialized

--- index.android.js
+++ index.android.js
@@ -1,52 +1,119 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
 var {
   AppRegistry,
+  Image,
+  ListView,
   StyleSheet,
   Text,
   View,
 } = React;
 
-var rnMaterialDesignGrid = React.createClass({
+var GridView = require('react-native-grid-view');
+
+var API_KEY = '7waqfqbprs7pajbz28mqf6vz';
+var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
+var PAGE_SIZE = 25;
+var PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
+var REQUEST_URL = API_URL + PARAMS;
+var MOVIES_PER_ROW = 3;
+
+var Movie = React.createClass({
   render: function() {
+      return <View style={styles.movie} >
+        <Image
+          source={{uri: this.props.movie.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
+        <View >
+          <Text 
+          style={styles.title}
+          numberOfLines={3}>{this.props.movie.title}</Text>
+          <Text style={styles.year}>{this.props.movie.year}</Text>
+        </View>
+      </View>
+  },
+});
+
+var AwesomeProject = React.createClass({
+  getInitialState: function() {
+    return {
+      dataSource: null,
+      loaded: false,
+    };
+  },
+
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: responseData.movies,
+          loaded: true,
+        });
+      })
+      .done();
+  },
+
+  render: function() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
+      <GridView
+        items={this.state.dataSource}
+        itemsPerRow={MOVIES_PER_ROW}
+        renderItem={this.renderItem}
+        style={styles.listView}
+      />
+    );
+  },
+
+  renderLoadingView: function() {
+    return (
+      <View>
+        <Text>
+          Loading movies...
         </Text>
       </View>
     );
-  }
+  },
+
+  renderItem: function(item) {
+      return <Movie movie={item} />
+  },
 });
 
 var styles = StyleSheet.create({
-  container: {
+  movie: {
+    height: 150,
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    flexDirection: 'column',
+  },
+  title: {
+    fontSize: 10,
+    marginBottom: 8,
+    width: 90,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  listView: {
+    paddingTop: 20,
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
-AppRegistry.registerComponent('rnMaterialDesignGrid', () => rnMaterialDesignGrid);
+AppRegistry.registerComponent('GridViewExample', () => AwesomeProject);
